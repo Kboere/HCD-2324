2c41e9b6c55c25d48f0dd2415db6e435c8846c50
Kevin Boere <kevin.boere@hva.nl>
2024-04-16 23:07:12 +0200

    test wth drag&drop

--- js/index.js
+++ js/index.js
@@ -1,53 +1,53 @@
 const selectedPoints = [];
 const body = document.body;
-const main = document.querySelector("main");
-const selectButton = document.getElementById("selectButton");
-const pasteButton = document.getElementById("pasteButton"); 
+let selectedInputField = null;
+let isTextSelectionActive = false; // Flag to track if text selection is active
 
-// Function to handle word selection
 function handleWordSelection(event) {
-    const range = document.caretRangeFromPoint(event.clientX, event.clientY);
-    const clickedPoint = range.cloneRange();
-    clickedPoint.collapse(true);
+    if (!body.classList.contains("dragging") && isTextSelectionActive) {
+        const range = document.caretRangeFromPoint(event.clientX, event.clientY);
+        const clickedPoint = range.cloneRange();
+        clickedPoint.collapse(true);
 
-    const firstClickIndicator = body.querySelector(".first-click-indicator");
+        const firstClickIndicator = body.querySelector(".first-click-indicator");
 
-    if (selectedPoints.length < 2) {
-        selectedPoints.push(clickedPoint);
+        if (selectedPoints.length < 2) {
+            selectedPoints.push(clickedPoint);
 
-        // If the indicator node doesn't exist, create and insert it
-        if (!firstClickIndicator) {
-            const newIndicator = document.createElement("span");
-            newIndicator.classList.add("first-click-indicator");
-            clickedPoint.insertNode(newIndicator);
-        }
-    }
-
-    if (selectedPoints.length === 2) {
-        // Remove the indicator node if it exists
-        if (firstClickIndicator) {
-            firstClickIndicator.remove();
+            // If the indicator node doesn't exist, create and insert it
+            if (!firstClickIndicator) {
+                const newIndicator = document.createElement("span");
+                newIndicator.classList.add("first-click-indicator");
+                clickedPoint.insertNode(newIndicator);
+            }
         }
 
-        // Create a range that spans the two selected points
-        const selectionRange = document.createRange();
-        selectionRange.setStart(selectedPoints[0].startContainer, selectedPoints[0].startOffset);
-        selectionRange.setEnd(selectedPoints[1].startContainer, selectedPoints[1].startOffset);
+        if (selectedPoints.length === 2) {
+            // Remove the indicator node if it exists
+            if (firstClickIndicator) {
+                firstClickIndicator.remove();
+            }
 
-        // Log the selected text to the console
-        const selectedText = selectionRange.toString();
-        console.log("Selected Text:", selectedText);
+            // Create a range that spans the two selected points
+            const selectionRange = document.createRange();
+            selectionRange.setStart(selectedPoints[0].startContainer, selectedPoints[0].startOffset);
+            selectionRange.setEnd(selectedPoints[1].startContainer, selectedPoints[1].startOffset);
 
-        // Call the function to copy the selected text
-        copyToClipboard(selectedText);
+            // Log the selected text to the console
+            const selectedText = selectionRange.toString();
+            console.log("Selected Text:", selectedText);
 
-        // Highlight the selection
-        const selection = window.getSelection();
-        selection.removeAllRanges();
-        selection.addRange(selectionRange);
+            // Call the function to copy the selected text
+            copyToClipboard(selectedText);
 
-        // Reset selection
-        selectedPoints.length = 0;
+            // Highlight the selection
+            const selection = window.getSelection();
+            selection.removeAllRanges();
+            selection.addRange(selectionRange);
+
+            // Reset selection
+            selectedPoints.length = 0;
+        }
     }
 }
 
@@ -72,39 +72,16 @@ function toggleWordSelection() {
         // Add event listener to allow word selection
         body.classList.add("selectable");
         body.addEventListener("click", handleWordSelection);
+        isTextSelectionActive = true;
         console.log("You can now select text within the body.");
     } else {
         // Remove event listener to prevent further selections
         body.classList.remove("selectable");
         body.removeEventListener("click", handleWordSelection);
+        isTextSelectionActive = false;
         console.log("Word selection within the body is disabled.");
     }
 }
-
-// Attach event listener to the select button
-selectButton.addEventListener("click", function (event) {
-    // Prevent the button click from being registered as the first click
-    event.stopPropagation();
-    // Toggle word selection
-    toggleWordSelection();
-});
-
-
-// TODO: make paste button work
-// TODO: create a function to handle pasting text
-// TODO: test if images can be copied and pasted
-
-let selectedInputField = null;
-
-// Add event listener to all input fields
-const inputFields = document.querySelectorAll(".inputField");
-inputFields.forEach(inputField => {
-    inputField.addEventListener("click", function () {
-        selectedInputField = this;
-
-        console.log(selectedInputField.dataset.textarea);
-    });
-});
 
 function pasteText() {
     if (selectedInputField) {
@@ -114,95 +91,104 @@ function pasteText() {
     }
 }
 
-if (pasteButton) {
-    pasteButton.addEventListener("click", function (event) {
+document.addEventListener('DOMContentLoaded', () => {
+    let dragSrcEl = null;
+
+    function handleDragStart(e) {
+        if (!isTextSelectionActive) {
+            this.style.opacity = '0.4';
+            dragSrcEl = this;
+            body.classList.add("dragging"); // Adding a class to body to indicate dragging
+            e.dataTransfer.effectAllowed = 'move';
+            e.dataTransfer.setData('text/html', this.innerHTML);
+        }
+    }
+
+    function handleDragEnd(e) {
+        this.style.opacity = '1';
+        body.classList.remove("dragging"); // Removing the dragging class from body
+        const items = document.querySelectorAll('.item');
+        items.forEach(item => item.classList.remove('over'));
+    }
+
+    function handleDragOver(e) {
+        if (e.preventDefault && !isTextSelectionActive) {
+            e.preventDefault();
+        }
+        if (!isTextSelectionActive) {
+            e.dataTransfer.dropEffect = 'move';
+        }
+        return false;
+    }
+
+    function handleDragEnter(e) {
+        if (!isTextSelectionActive) {
+            this.classList.add('over');
+        }
+    }
+
+    function handleDragLeave(e) {
+        if (!isTextSelectionActive) {
+            this.classList.remove('over');
+        }
+    }
+
+    function handleDrop(e) {
+        if (e.stopPropagation && !isTextSelectionActive) {
+            e.stopPropagation(); // stops the browser from redirecting.
+        }
+
+        if (!isTextSelectionActive && dragSrcEl != this) {
+            dragSrcEl.innerHTML = this.innerHTML;
+            this.innerHTML = e.dataTransfer.getData('text/html');
+        }
+
+        return false;
+    }
+
+    const items = document.querySelectorAll('.item');
+    items.forEach(item => {
+        item.addEventListener('dragstart', handleDragStart, false);
+        item.addEventListener('dragenter', handleDragEnter, false);
+        item.addEventListener('dragover', handleDragOver, false);
+        item.addEventListener('dragleave', handleDragLeave, false);
+        item.addEventListener('drop', handleDrop, false);
+        item.addEventListener('dragend', handleDragEnd, false);
+    });
+
+    const menuBtn = document.getElementById("menuToggle");
+    const header = document.querySelector("header");
+
+    menuBtn.addEventListener("click", function(event) {
+        header.classList.toggle("open");
+        event.stopPropagation();
+    });
+
+    document.addEventListener("keyup", function(event) {
+        // Check if the 't' key was pressed
+        if (event.key === "t") {
+            header.classList.toggle("open");
+        }
+    });
+
+    const selectButton = document.getElementById("selectButton");
+    selectButton.addEventListener("click", function(event) {
+        event.stopPropagation();
+        toggleWordSelection();
+    });
+
+    const pasteButton = document.getElementById("pasteButton");
+    pasteButton.addEventListener("click", function(event) {
+        event.stopPropagation();
         pasteText();
     });
-}
 
-const menuBtn = document.getElementById("menuToggle");
-const header = document.querySelector("header");
-
-menuBtn.addEventListener("click", function() {
-    header.classList.toggle("open");
-    // if (header.classList.contains("open")) {
-    //     main.style.width = "calc(100dvw + 50%)";
-    // } else {
-    //     main.style.width = "100dvw";
-    // }
-    event.stopPropagation();
-});
-
-document.addEventListener("keyup", function(event) {
-    // Check if the 't' key was pressed
-    if (event.key === "t") {
-        header.classList.toggle("open");
-    }
-});
-
-
-
-
-document.addEventListener('DOMContentLoaded', (event) => {
-
-  var dragSrcEl = null;
-  
-  function handleDragStart(e) {
-    this.style.opacity = '0.4';
-    
-    dragSrcEl = this;
-
-    e.dataTransfer.effectAllowed = 'move';
-    e.dataTransfer.setData('text/html', this.innerHTML);
-  }
-
-  function handleDragOver(e) {
-    if (e.preventDefault) {
-      e.preventDefault();
-    }
-
-    e.dataTransfer.dropEffect = 'move';
-    
-    return false;
-  }
-
-  function handleDragEnter(e) {
-    this.classList.add('over');
-  }
-
-  function handleDragLeave(e) {
-    this.classList.remove('over');
-  }
-
-  function handleDrop(e) {
-    if (e.stopPropagation) {
-      e.stopPropagation(); // stops the browser from redirecting.
-    }
-    
-    if (dragSrcEl != this) {
-      dragSrcEl.innerHTML = this.innerHTML;
-      this.innerHTML = e.dataTransfer.getData('text/html');
-    }
-    
-    return false;
-  }
-
-  function handleDragEnd(e) {
-    this.style.opacity = '1';
-    
-    items.forEach(function (item) {
-      item.classList.remove('over');
+    // Add event listener to all input fields
+    const inputFields = document.querySelectorAll(".inputField");
+    inputFields.forEach(inputField => {
+        inputField.addEventListener("click", function() {
+            selectedInputField = this;
+            console.log(selectedInputField.dataset.textarea);
+        });
     });
-  }
-  
-  
-  let items = document.querySelectorAll('section');
-  items.forEach(function(item) {
-    item.addEventListener('dragstart', handleDragStart, false);
-    item.addEventListener('dragenter', handleDragEnter, false);
-    item.addEventListener('dragover', handleDragOver, false);
-    item.addEventListener('dragleave', handleDragLeave, false);
-    item.addEventListener('drop', handleDrop, false);
-    item.addEventListener('dragend', handleDragEnd, false);
-  });
 });
